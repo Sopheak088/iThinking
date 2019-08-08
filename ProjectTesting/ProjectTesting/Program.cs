a568341e7b18b75850912ac80731b90f6433fbec
Sopheak088 <ourlesson031@gmail.com>
2019-08-08 14:33:17 +0800

    samang commit 02

--- ProjectTesting/ProjectTesting/Program.cs
+++ ProjectTesting/ProjectTesting/Program.cs
@@ -19,7 +19,7 @@ namespace ProjectTesting
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmSupplierList());
+            Application.Run(new FrmProductList());
         }
     }
 }
