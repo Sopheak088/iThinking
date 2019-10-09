9f1209edf4c6955066ac00d3da481eeafd214a60
Sopheak088 <ourlesson031@gmail.com>
2019-10-09 10:37:56 +0700

    correct list customer

--- ProjectTesting/ProjectManagement/Customer/View/frmCustomerList.Designer.cs
+++ ProjectTesting/ProjectManagement/Customer/View/frmCustomerList.Designer.cs
@@ -361,10 +361,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.ControlBox = false;
             this.Controls.Add(this.uiPanel3);
             this.Controls.Add(this.uiPanel2);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmCustomerList";
             this.Text = "CustomerList";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
